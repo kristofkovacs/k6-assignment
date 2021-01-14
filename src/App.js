4b3f3d3d4d516adab59ae08be0064fa4e24e4edf
Kristof Kovacs-Egri <kristof@serieseight.com>
2021-01-15 00:29:33 +0200

    Pagination added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React, { useEffect, useState } from "react";
-import { Flex, ChakraProvider, useDisclosure } from "@chakra-ui/react";
+import {
+  Flex,
+  Button,
+  ChakraProvider,
+  Spinner,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { ChevronLeftIcon, ChevronRightIcon } from "@chakra-ui/icons";
 import starwars from "./api/starwars";
 
 import SearchBar from "./components/SearchBar";
 import List from "./components/List";
-import Detail from "./components/Detail";
 
 const App = () => {
   const [term, setTerm] = useState("");
   const [debouncedTerm, setDebouncedTerm] = useState(term);
   const [characters, setCharacters] = useState([]);
+  const [previous, setPrevious] = useState(null);
+  const [next, setNext] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   const onSearch = (term) => {
     setTerm(term);
@@ -27,15 +36,27 @@ const App = () => {
 
   useEffect(() => {
     starwars.get("/people", { params: { search: term } }).then(({ data }) => {
-      setCharacters(data.results);
-      console.log(data);
+      const { previous, next, results } = data;
+      setPrevious(previous);
+      setNext(next);
+      setCharacters(results);
+      setLoading(false);
     });
   }, [debouncedTerm]);
 
+  const pageChange = (link) => {
+    console.log(link);
+    setLoading(true);
+    starwars.get(link, { params: { search: term } }).then(({ data }) => {
+      const { previous, next, results } = data;
+      setPrevious(previous);
+      setNext(next);
+      setCharacters(results);
+      setLoading(false);
+    });
+  };
+
   useEffect(() => {}, []);
-
-  const { onClose } = useDisclosure();
-
   return (
     <ChakraProvider>
       <Flex justify="center" w="100vw" minH="100vh" bgColor="gray.50">
@@ -46,9 +67,43 @@ const App = () => {
           direction="column"
         >
           <SearchBar onSearch={onSearch} />
-          <List mt={6} data={characters} />
-
-          {/* <Detail isOpen={true} onClose={onClose} /> */}
+          {loading ? (
+            <Flex
+              justify="center"
+              align="center"
+              justifySelf="center"
+              alignSelf="center"
+              py={12}
+            >
+              <Spinner
+                thickness="4px"
+                speed="0.5s"
+                emptyColor="gray.200"
+                color="teal.500"
+                size="lg"
+                mt={64}
+              />
+            </Flex>
+          ) : (
+            <List mt={6} data={characters} />
+          )}
+          <Flex mt={8}>
+            <Button
+              mr={4}
+              leftIcon={<ChevronLeftIcon />}
+              isDisabled={!previous}
+              onClick={(e) => pageChange(previous)}
+            >
+              Previous page
+            </Button>
+            <Button
+              rightIcon={<ChevronRightIcon />}
+              isDisabled={!next}
+              onClick={(e) => pageChange(next)}
+            >
+              Next page
+            </Button>
+          </Flex>
         </Flex>
       </Flex>
     </ChakraProvider>
